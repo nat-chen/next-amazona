3b26384be35ab99e08aee098a3fe050369328e49
Nat Chen <chen_natalie@outlook.com>
2021-10-23 17:39:26 +0800

    create app context

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import Head from 'next/head';
-import NextLink from 'next/link';
-import { AppBar, Toolbar, Typography, Container, Link, createTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
-import useStyles from '../utils/styles';
+import React, { useContext } from "react";
+import Head from "next/head";
+import NextLink from "next/link";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+} from "@material-ui/core";
+import useStyles from "../utils/styles";
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
 
 export default function Layout({ title, description, children }) {
-  const theme = createTheme({
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createMuiTheme({
     typography: {
       h1: {
         fontSize: '1.6rem',
@@ -17,22 +31,27 @@ export default function Layout({ title, description, children }) {
         fontWeight: 400,
         margin: '1rem 0',
       },
-      palette: {
-        type: 'light',
-        primary: {
-          main: '#f0c000',
-        },
-        secondary: {
-          main: '#208080',
-        }
-      }
-    }
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
   });
   const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
   return (
     <div>
       <Head>
-        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        <title>{title ? `${title} - Next Amazona` : "Next Amazona"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
       <ThemeProvider theme={theme}>
@@ -46,6 +65,10 @@ export default function Layout({ title, description, children }) {
             </NextLink>
             <div className={classes.grow}></div>
             <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
               <NextLink href="/cart" passHref>
                 <Link>Cart</Link>
               </NextLink>
@@ -56,12 +79,10 @@ export default function Layout({ title, description, children }) {
           </Toolbar>
         </AppBar>
       </ThemeProvider>
-      <Container className={classes.main}>
-        {children}
-      </Container>
+      <Container className={classes.main}>{children}</Container>
       <footer className={classes.footer}>
         <Typography>All rights reserved. Next Amazona</Typography>
       </footer>
     </div>
-  )
+  );
 }
