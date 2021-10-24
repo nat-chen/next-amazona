0d00f5019afd6548e13b3ce1c3d1d0004d1c7cdd
Nat Chen <chen_natalie@outlook.com>
2021-10-24 16:07:38 +0800

    implement add to cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
-import React from 'react';
-import NextLink from 'next/link';
-import Image from 'next/image';
+import React, { useContext } from "react";
+import NextLink from "next/link";
+import Image from "next/image";
 import {
   Grid,
   Link,
@@ -9,18 +9,29 @@ import {
   Typography,
   Card,
   Button,
-} from '@material-ui/core';
-import Layout from '../../components/Layout';
-import useStyles from '../../utils/styles';
-import Product from '../../models/Product';
-import db from '../../utils/db';
+} from "@material-ui/core";
+import Layout from "../../components/Layout";
+import useStyles from "../../utils/styles";
+import Product from "../../models/Product";
+import db from "../../utils/db";
+import axios from "axios";
+import { Store } from "../../utils/Store";
 
 export default function ProductScreen(props) {
+  const { dispatch } = useContext(Store);
   const { product } = props;
   const classes = useStyles();
   if (!product) {
-    return <div>Product Not Found</div>
+    return <div>Product Not Found</div>;
   }
+  const addToCartHandler = async () => {
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock <= 0) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+  };
   return (
     <Layout title={product.name} description={product.description}>
       <div className={classes.section}>
@@ -41,9 +52,11 @@ export default function ProductScreen(props) {
           ></Image>
         </Grid>
         <Grid item md={3} xs={12}>
-        <List>
+          <List>
             <ListItem>
-              <Typography component="h1" variant="h1">{product.name}</Typography>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
             </ListItem>
             <ListItem>
               <Typography>Category: {product.category}</Typography>
@@ -63,7 +76,7 @@ export default function ProductScreen(props) {
         </Grid>
         <Grid item md={3} xs={12}>
           <Card>
-          <List>
+            <List>
               <ListItem>
                 <Grid container>
                   <Grid item xs={6}>
@@ -81,13 +94,18 @@ export default function ProductScreen(props) {
                   </Grid>
                   <Grid item xs={6}>
                     <Typography>
-                      {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
+                      {product.countInStock > 0 ? "In stock" : "Unavailable"}
                     </Typography>
                   </Grid>
                 </Grid>
               </ListItem>
               <ListItem>
-                <Button fullWidth variant="contained" color="primary">
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={addToCartHandler}
+                >
                   Add to cart
                 </Button>
               </ListItem>
@@ -96,11 +114,11 @@ export default function ProductScreen(props) {
         </Grid>
       </Grid>
     </Layout>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
-  const { params } =context;
+  const { params } = context;
   const { slug } = params;
   await db.connect();
   const product = await Product.findOne({ slug }).lean();
@@ -108,6 +126,6 @@ export async function getServerSideProps(context) {
   return {
     props: {
       product: db.convertDocToObj(product),
-    }
-  }
+    },
+  };
 }
