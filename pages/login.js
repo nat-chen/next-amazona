d60669dc18c671ecd4e9d7ecfd562c0753f21b72
Nat Chen <chen_natalie@outlook.com>
2021-10-27 09:59:08 +0800

    complete login page

--- pages/login.js
+++ pages/login.js
@@ -7,12 +7,24 @@ import {
   Link,
 } from '@material-ui/core';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import NextLink from 'next/link';
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
 import useStyles from '../utils/styles';
+import Cookies from 'js-cookie';
 
 export default function Login() {
+  const router = useRouter();
+  const { redirect } = router.query; // login?redirect=/shipping
+  const { state, dispatch } = useContext(Store);
+  const { userInfo } = state;
+  useEffect(() => {
+    if (userInfo) {
+      router.push('/');
+    }
+  }, []);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const classes = useStyles();
@@ -23,7 +35,9 @@ export default function Login() {
         email,
         password
       });
-      alert('success login');
+      dispatch({ type: 'USER_LOGIN', payload: data });
+      Cookies.set('userInfo', JSON.stringify(data));
+      router.push(redirect || '/');
     } catch (err) {
       alert(err.response.data ? err.response.data.message : err.message);
     }
