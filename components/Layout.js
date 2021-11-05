fcfb5f7b1c78c89e072929a07242f0af96c59958
Nat Chen <chen_natalie@outlook.com>
2021-11-05 09:51:47 +0800

    display orders history

--- components/Layout.js
+++ components/Layout.js
@@ -59,8 +59,11 @@ export default function Layout({ title, description, children }) {
   const loginClickHandler = (e) => {
     setAnchorEl(e.currentTarget);
   }
-  const loginMenuCloseHandler = () => {
+  const loginMenuCloseHandler = (e, redirect) => {
     setAnchorEl(null);
+    if (redirect) {
+      router.push(redirect);
+    }
   };
   const logoutClickHandler = () => {
     setAnchorEl(null);
@@ -117,8 +120,14 @@ export default function Layout({ title, description, children }) {
                     open={Boolean(anchorEl)}
                     onClose={loginMenuCloseHandler}
                   >
-                    <MenuItem onClick={loginMenuCloseHandler}>Profile</MenuItem>
-                    <MenuItem onClick={loginMenuCloseHandler}>
+                    <MenuItem
+                      onClick={(e) => loginMenuCloseHandler(e, '/profile')}
+                    >
+                      Profile
+                    </MenuItem>
+                    <MenuItem
+                      onClick={(e) => loginMenuCloseHandler(e, '/order-history')
+                    }>
                       My account
                     </MenuItem>
                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
